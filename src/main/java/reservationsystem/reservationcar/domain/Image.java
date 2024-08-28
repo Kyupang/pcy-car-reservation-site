314bbc27b623ec4fc82e028cf71e0df9808ffc7d
KimKyuHwan <vkdls754@gmail.com>
2024-08-28 19:45:29 +0900

    feat(Domain): 차량, 예약, 게시판, 경고문 도메인 클래스 구현

---
+++ src/main/java/reservationsystem/reservationcar/domain/Image.java
@@ -0,0 +1,27 @@
+package reservationsystem.reservationcar.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
+
