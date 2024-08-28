30a11078d477669b777e5462bf7a253523c9bacd
KimKyuHwan <vkdls754@gmail.com>
2024-08-28 19:46:31 +0900

    feat(Repository): 차량, 이미지, 게시물, 예약, 경고문 JPA 리포지토리 클래스 구현

---
+++ src/main/java/reservationsystem/reservationcar/repository/CarRepository.java
@@ -0,0 +1,40 @@
+package reservationsystem.reservationcar.repository;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import reservationsystem.reservationcar.domain.Car;
+
+@Repository
+@RequiredArgsConstructor
+public class CarRepository {
+    private final EntityManager em;
+
+    public void save(Car car) {
+        em.persist(car);
+    }
+
+    public Car findOne(Long id) {
+        return em.find(Car.class, id);
+    }
+
+//    public void delete(Long id) {
+//        Car car = em.find(Car.class, id);
+//
+//        if (car != null) {
+//            em.remove(car);
+//        }
+//    }
+
+    public List<Car> findAll() {
+        return em.createQuery("select c from Car c", Car.class)
+                .getResultList();
+    }
+
+//    public Car findByName(String name) {
+//        return em.createQuery("select c from Car c where c.name = :name", Car.class)
+//                .setParameter("name", name)
+//                .getResultList();
+//    }
+}
